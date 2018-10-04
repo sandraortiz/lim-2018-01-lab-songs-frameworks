1f92866c6da000d32d23dd06403938c3d601a4fb
sandraortiz <rocio12vx@gmail.com>
2018-10-04 15:52:16 -0500

    Updates

---
+++ precache-manifest.f6097900e2abf73483d9fff7dda44f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "6ffe11c053c3ca004617",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.6ffe11c0.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "6ffe11c053c3ca004617",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.4f137df0.chunk.css"
+  },
+  {
+    "revision": "89da5aed2005ff6b5abfc2e2c983b628",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
