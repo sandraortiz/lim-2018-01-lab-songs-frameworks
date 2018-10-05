bc6949d74fc51ed7c6be955f2120f4a621dec85b
sandraortiz <rocio12vx@gmail.com>
2018-10-05 18:34:49 -0500

    fetch react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  super() 
+    this.state = {
+      data:[],
+    }
+    this.getData();
+  
+} 
+getData() {
+fetch('http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=f36f678dce4b7175b8c9e6e14507ac28&format=json&mbid=251f6d72-a1d9-4b9d-944a-2df9b26f6212')
+.then((response)=>{
+  return response.json()
+})
+.then((data))
+
+
+//  .then((resp)=>{
+//    resp.json().then((res) =>{
+//      console.log(res.track);
+//      console.log(res.track.name);
+//      this.setState({data:res.track})
+//    } )
+//  } )
+
+} 
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      {
+        
+ <h3>data</h3>
+      }
+    </div>
+     )
+    }
   }
-}
-
 export default App;
