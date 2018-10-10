a4080d6ee376836d8c95a2e587cea648f63d7558
sandraortiz <rocio12vx@gmail.com>
2018-10-10 10:24:17 -0500

    artistas

--- src/App.js
+++ src/App.js
@@ -1,42 +1,125 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import profiles from './profiles';
+import { Container, Row, Col , Button } from 'mdbreact';
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
 
-class App extends Component {
-constructor(){
-  super() 
     this.state = {
       data:[],
+      index: 0,
+      disabledNext: false,
+      disabledPrev: false
     }
-    this.getData();
-  
-} 
-getData() {
-fetch('http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=f36f678dce4b7175b8c9e6e14507ac28&format=json&mbid=251f6d72-a1d9-4b9d-944a-2df9b26f6212')
-.then((response)=>{
-  return response.json()
-})
-.then((data))
 
 
-//  .then((resp)=>{
-//    resp.json().then((res) =>{
-//      console.log(res.track);
-//      console.log(res.track.name);
-//      this.setState({data:res.track})
-//    } )
-//  } )
-
-} 
-  render() {
-    
-    return (
-      <div className="App">
-      {
-        
- <h3>data</h3>
-      }
-    </div>
-     )
-    }
   }
+  
+  togglePrev(e) {
+    let index = this.state.index - 1;
+    let disabledPrev = (index === 0);
+
+    this.setState({ index: index, disabledPrev: disabledPrev, disabledNext: false })
+  }
+
+ toggleNext(e) {
+     let index = this.state.index + 1;
+     let disabledNext = index === (this.props.profiles.length - 1);
+
+     this.setState({ index: index, disabledNext: disabledNext, disabledPrev: false })
+   }
+
+render() {
+     const { index, disabledNext, disabledPrev } = this.state
+     const profile = this.props.profiles ? this.props.profiles[index] : null
+     
+     if (profile) {
+       return (
+         <div className='profile'>
+                
+           <Profile {...profile} />
+           <div>
+             <Prev toggle={(e) => this.togglePrev(e)} active={disabledPrev} />
+             <Next toggle={(e) => this.toggleNext(e)} active={disabledNext} />
+           
+           </div>
+         
+         </div>
+       )
+     } else {
+       return <span>error</span>
+     }
+  }
+
+  
+}
+
+function Prev(props) {
+  return ( 
+    <Container>
+    <Row>
+      <Col md="6">
+    <Button onClick={props.toggle} disabled={props.active}>Previous</Button>
+    </Col>
+        </Row>
+      </Container>
+    
+  );
+}
+
+function Next(props) {
+  return (
+    <Container>
+    <Row>
+      <Col md="6">
+    <Button onClick={props.toggle} disabled={props.active}>Next</Button>
+
+    </Col>
+        </Row>
+      </Container>
+  );
+}
+
+
+
+function Profile(props) {
+  const data = [] ;
+  fetch(props.songs)
+  .then(response =>  response.json() )
+  //  .then(text =>  console.log(text.toptracks.track))
+   .then(artistsData => {
+    Object.values(artistsData).forEach(artistData => {
+      data.push({
+        artistData: artistData.track
+      })
+     console.log(data);
+    })
+  })
+  return (
+
+
+    <Container>
+        <Row>
+          <Col md="6">
+            <form>
+              <p className="h5 text-center mb-4">{props.name}</p>
+              
+              <div className="text-center">
+              <img src= {props.image} className="mx-auto d-block"/> 
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    
+  );
+}
+
+const App = () => (
+  <div>
+    <Main profiles={profiles} />
+  
+  </div>
+)
+
 export default App;
